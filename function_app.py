3c311ef41c994945c5173553579edf9f3486eca2
Josh McCurry <joshmccurry@microsoft.com>
2025-01-07 12:33:46 -0500

    Initial Commit

---
+++ function_app.py
@@ -0,0 +1,12 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.blob_trigger(arg_name="myblob", path="inputcontainer", connection="BlobTriggerStorage") 
+def BlobTrigger(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
