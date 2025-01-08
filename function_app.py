c7e32888e2357eb065d52a8689c8e73f4fc771f6
Josh McCurry <joshmccurry@microsoft.com>
2025-01-08 08:51:20 -0500

    Update to BlobClient Rather Than InputStream

--- function_app.py
+++ function_app.py
@@ -1,12 +1,20 @@
 import azure.functions as func
+import azurefunctions.extensions.bindings.blob as blob
 import datetime
 import json
 import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="inputcontainer", connection="BlobTriggerStorage") 
-def BlobTrigger(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
+@app.blob_trigger(arg_name="client", path="inputcontainer", connection="BlobTriggerStorage") 
+#def BlobTrigger(myblob: func.InputStream):
+def BlobTrigger(client: blob.BlobClient):
+    #logging.info(f"Python blob trigger function processed blob"
+    #            f"Name: {myblob.name}"
+    #            f"Blob Size: {myblob.length} bytes")
+    logging.info(
+        f"Python blob trigger function processed blob \n"
+        f"Properties: {client.get_blob_properties()}\n"
+        f"Blob content head: {client.download_blob().read(size=1)}"
+    )
+
